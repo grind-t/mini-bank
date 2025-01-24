8de9bbc42a35ac05c15fbb416d536442ce959ee9
grind-t <grindt.gd@gmail.com>
2025-01-25 00:06:52 +0300

    add mockup

---
+++ client/eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["src-tauri/", ...includeIgnoreFile(gitignorePath).ignores] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];

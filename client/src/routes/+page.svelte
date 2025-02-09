77788cdabdb2eb3130d5800d700d431db07d0f0f
grind-t <grindt.gd@gmail.com>
2025-02-09 15:42:32 +0300

    update

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
   import { onMount } from "svelte";
 
   onMount(async () => {
-    const greetMsg = await invoke("greet", { name: "world" });
-    console.log(greetMsg);
+    //const greetMsg = await invoke("greet", { name: "world" });
+    //console.log(greetMsg);
   });
 </script>
 
 <main class="flex flex-col">
-  <input class="btn w-fit m-2" type="radio" name="asset-type" value="bonds" aria-label="Облигации" />  
-
-  <select class="select m-2">
-    <option disabled selected>Счет</option>
-  </select>
-
-  <input class="input m-2" type="text" placeholder="Сумма" />
-
-  <input class="input m-2" type="datetime-local" placeholder="Конец"/>
+  <input
+    class="input m-2"
+    type="text"
+    placeholder="Сумма на {Intl.DateTimeFormat('ru-RU', {
+      month: 'long',
+    }).format(Date.now())}"
+  />
 
   <ul class="list">
-    {#each {length: 8} as _, i}
+    {#each { length: 8 } as _, i}
       <li class="list-row">Облигация {i + 1}</li>
     {/each}
   </ul>

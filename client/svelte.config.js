5624b85bf61b122fcb4f25a1104d94c9f4bb8838
grind-t <grindt.gd@gmail.com>
2025-02-20 23:58:51 +0300

    add gh-pages

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -2,13 +2,19 @@
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 import adapter from "@sveltejs/adapter-static";
-import { Config } from "@sveltejs/kit";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const config: Config = {
+const dev = process.env.NODE_ENV === "development";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    paths: {
+      base: dev ? "" : "/mini-bank",
+    },
+    appDir: "internal",
   },
 };
 

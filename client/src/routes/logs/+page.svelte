259ea08637ea66d318df2fb442810966c23b86ab
grind-t <grindt.gd@gmail.com>
2025-04-12 01:32:56 +0300

    refactor: logs

--- client/src/routes/logs/+page.svelte
+++ client/src/routes/logs/+page.svelte
@@ -1,43 +1,21 @@
 <script lang="ts">
-  import { getBonds, getDCAStrategyLogs } from "$lib/common/api";
-  import { toRecord } from "@grind-t/toolkit";
+  import { getDCAStrategyLogs } from "$lib/common/api";
   import dayjs from "dayjs";
   import Log from "./Log.svelte";
 
-  async function getData() {
-    const logs = await getDCAStrategyLogs({
+  function getLogs() {
+    return getDCAStrategyLogs({
       id: "bonds",
       from: dayjs().subtract(1, "week").toDate(),
       to: dayjs().toDate(),
     });
-
-    const whitelist = logs.reduce((set, log) => {
-      log.strategy.assets.forEach((v) => {
-        set.add(v.isin);
-      });
-
-      return set;
-    }, new Set<string>());
-
-    const bonds = await getBonds({
-      filter: {
-        whitelist: Array.from(whitelist),
-        yield: {
-          lte: -1,
-        },
-      },
-    });
-
-    return [logs, toRecord(bonds, (v) => v.isin)] as const;
   }
 </script>
 
 <div class="flex flex-col h-full">
-  {#await getData() then [logs, bonds]}
-    {@const getAssetBody = (isin: string) => bonds[isin]}
-
+  {#await getLogs() then logs}
     {#each logs as log}
-      <Log {log} {getAssetBody} />
+      <Log {log} />
     {:else}
       Логи отсутсвуют
     {/each}
--- client/src/routes/logs/Log.svelte
+++ client/src/routes/logs/Log.svelte
@@ -2,27 +2,14 @@
   import type { DCAStrategyLog } from "$lib/common/api";
   import dayjs from "dayjs";
 
-  let {
-    log,
-    getAssetBody,
-  }: { log: DCAStrategyLog; getAssetBody: (isin: string) => { name: string } } =
-    $props();
-
-  function getAsset(orderedAsset: (typeof log.orderedAssets)[number]) {
-    const idx = log.initialAssets.findIndex((v) => v.id === orderedAsset.id);
-    const isin = log.strategy.assets[idx].isin;
-    const body = getAssetBody(isin);
-    return { ...body, ...orderedAsset };
-  }
+  let { log }: { log: DCAStrategyLog } = $props();
 </script>
 
 <div class="divider">{dayjs(log.timestamp).format("DD.MM.YYYY HH:mm")}</div>
 
-{#each log.orderedAssets as orderedAsset}
-  {@const asset = getAsset(orderedAsset)}
-
+{#each log.orderedAssets as asset}
   <div class="chat chat-start">
-    {#if "error" in orderedAsset}
+    {#if "error" in asset}
       <div class="chat-bubble chat-bubble-error">
         Не удалось приобрести {asset.name} ({asset.error})
       </div>

eaeb3de0aebd82725fd91dbf24abe02f66a189d8
grind-t <grindt.gd@gmail.com>
2025-04-27 21:39:55 +0300

    add catch-all for static

--- server/src/features/app/client/middleware.ts
+++ server/src/features/app/client/middleware.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import path from "path";
 
-export const clientMiddleware = express.static(
-  path.join(import.meta.dirname, "build")
-);
+const buildPath = path.join(import.meta.dirname, "build");
+const indexPath = path.join(buildPath, "index.html");
+
+export const clientMiddleware = [
+  express.static(buildPath),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.method === "GET") {
+      res.sendFile(indexPath);
+    } else {
+      next();
+    }
+  },
+];
